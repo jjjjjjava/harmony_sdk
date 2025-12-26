c914ccba0d4b960f9d91daef4bee6b905cefd03e
panruiqi <panruiqi@ovopark.com>
2025-12-26 11:05:19 +0800

    Remove libraries, prepare for submodule structure

--- lib_ffmpeg_utils/src/main/cpp/fftools/exception.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <setjmp.h>
-
-/** 线程局部存储的跳转缓冲区 */
-__thread jmp_buf ex_buf__;
--- lib_ffmpeg_utils/src/main/cpp/fftools/exception.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXCEPTION_H
-#define EXCEPTION_H
-
-#include <setjmp.h>
-
-/** 线程局部存储的跳转缓冲区，避免多线程冲突 */
-extern __thread jmp_buf ex_buf__;
-
-#endif // EXCEPTION_H
